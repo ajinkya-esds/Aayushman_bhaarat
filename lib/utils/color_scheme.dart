2f65dbe0f9f493ae36f9c79457b3cbfacb2e06b0
Ajinkya Aher <ajinkya.aher@esds.co.in>
2025-01-08 17:19:45 +0530

    Language screen design added.

---
+++ lib/utils/color_scheme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color appbarTitleColor = const Color(0xff1E1E1E);
+Color primaryColor = const Color(0xff11B3CF);
+Color secondaryColor = const Color(0xffF21359);
